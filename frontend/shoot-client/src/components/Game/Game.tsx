92d88f4ed0173f71d8e557da223cc67207b0a45a
deadwing88 <timbo145@gmail.com>
2021-03-19 23:07:29 -0400

    setting up layout

--- frontend/shoot-client/src/components/Game/Game.tsx
+++ frontend/shoot-client/src/components/Game/Game.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import GameControls from "./GameControls";
 import SceneComponent from "./SceneComponent";
 import { onSceneReady, onRender } from "./SceneFunctions";
+import "./Game.css";
 
 const Game: React.FC = () => {
-
-    const divStyle = {
-        height: '100%',
-        width: '100%',
-      };
       
     return (  
-        <div>
-            <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" style={divStyle} />
+        <div className="game">
+            <div className="row">
+                <div className="column-3">
+                    <SceneComponent className="scene" antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" />
+                </div>
+                <div className="column-1">
+                    <GameControls></GameControls>
+                </div>
+            </div>
         </div>
     );
 };
