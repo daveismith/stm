0199676a4e32309733ca1bfef1d05ebac38838b7
David Smith <david.smith@mmbnetworks.com>
2021-03-23 18:35:55 -0400

    Work In Progress

--- backend/ShootTheMoon/ShootTheMoon/Program.cs
+++ backend/ShootTheMoon/ShootTheMoon/Program.cs
@@ -7,13 +7,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Grpc.Core;
+using My.Sample.Namespace;
+
 namespace ShootTheMoon
 {
+
+    class ShootServerImpl : ShootServer.ShootServerBase
+    {
+
+        public override async Task JoinGame(JoinGameRequest request, IServerStreamWriter<Notification> responseStream, ServerCallContext context)
+        {
+            var rng = new Random();
+            var now = DateTime.UtcNow;
+
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested && i < 20)
+            {
+                await Task.Delay(500); // Gotta look busy
+
+
+                StatusResponse r = new StatusResponse();
+                r.Success = true;
+                r.ErrorNum = 0;
+                r.ErrorText = "";
+
+                Notification n = new Notification();
+                n.Status = r;
+
+                await responseStream.WriteAsync(n);                
+            }
+        }
+
+    }
+
     public class Program
     {
+        const int Port = 30051;
+
         public static void Main(string[] args)
         {
+
+            Server server = new Server
+            {
+                Services = { ShootServer.BindService(new ShootServerImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+
             CreateHostBuilder(args).Build().Run();
+
+            server.ShutdownAsync().Wait();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- frontend/shoot-client/src/App.tsx
+++ frontend/shoot-client/src/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import * as grpcWeb from 'grpc-web';
+import {ShootServerClient} from './ShootServiceClientPb';
+import {JoinGameRequest, Notification} from './shoot_pb';
 import logo from './logo.svg';
 import './App.css';
 
+const stmService = new ShootServerClient('http://localhost:8080', null, null);
+
 function App() {
+
+  const request = new JoinGameRequest();
+  request.setId('1234');
+
+  const stream = stmService.joinGame(request, {} ) as grpcWeb.ClientReadableStream<Notification>;
+
+  stream.on('data', (response: Notification) => {
+    if (response.hasStatus()) {
+      console.log('status: ');
+      console.log(response.getStatus()?.toObject());
+    } else {
+      console.log('data');
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
