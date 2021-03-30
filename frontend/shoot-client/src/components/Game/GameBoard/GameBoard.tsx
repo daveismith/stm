40da546215f03b9e83c203e6e7e1e9835a0fe8a0
deadwing88 <timbo145@gmail.com>
2021-03-30 19:51:39 -0400

    starting on cards

--- frontend/shoot-client/src/components/Game/GameBoard/GameBoard.tsx
+++ frontend/shoot-client/src/components/Game/GameBoard/GameBoard.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import PlayingCard from "../../Common/PlayingCard";
 
 const GameBoard: React.FC = () => {
       
     return (  
         <div style={{color: 'white', backgroundColor: '#404040', height: '100%'}}>
-            This is the GameBoard Component
+            <PlayingCard suit="spades" rank="10"></PlayingCard>
+            <PlayingCard suit="hearts" rank="A"></PlayingCard>
+            <PlayingCard suit="clubs" rank="Q"></PlayingCard>
         </div>
     );
 };
