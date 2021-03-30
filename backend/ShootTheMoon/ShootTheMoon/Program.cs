460cf7d42d6e8d2188dedad837b7a2861e2644d0
David Smith <david.smith@mmbnetworks.com>
2021-03-30 19:48:35 -0400

    Updates based on progress I've been making. Still requires manual generation of the shoot_pb.js, shoot_pb.d.ts and ShootServiceClientPb.ts files. The command is 'protoc -I ../../schema shoot.proto --js_out=import_style=commonjs,binary:src --grpc-web_out=import_style=typescript,mode=grpcwebtext:src' when in the frontent/shoot-client directory.

--- backend/ShootTheMoon/ShootTheMoon/Program.cs
+++ backend/ShootTheMoon/ShootTheMoon/Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 using Grpc.Core;
 using My.Sample.Namespace;
+using ShootTheMoon.Game;
 
 namespace ShootTheMoon
 {
@@ -16,28 +17,125 @@ namespace ShootTheMoon
     class ShootServerImpl : ShootServer.ShootServerBase
     {
 
+        class RpcClient {
+
+            public IServerStreamWriter<Notification> Stream { get; }
+
+            public Client Client { get; }
+
+            public Game.Game Game { get; }
+
+            public RpcClient(IServerStreamWriter<Notification> stream, Client client, Game.Game game) {
+                Stream = stream;
+                Client = client;
+                Game  = game;
+            }
+
+        }
+
+        Dictionary<string, Game.Game> games = new Dictionary<string, Game.Game>();
+        Dictionary<string, RpcClient> clients = new Dictionary<string, RpcClient>();
+
         public override async Task JoinGame(JoinGameRequest request, IServerStreamWriter<Notification> responseStream, ServerCallContext context)
         {
-            var rng = new Random();
-            var now = DateTime.UtcNow;
+            Game.Game game;
 
-            var i = 0;
-            while (!context.CancellationToken.IsCancellationRequested && i < 20)
-            {
-                await Task.Delay(500); // Gotta look busy
+            try {
+                game = games[request.Uuid];
+            } catch (KeyNotFoundException) {
+                // The game key doesn't exist, so try to add the game
+                game = new Game.Game(GameSettings.GamePresets["SIXPLAYER"]);
+                games.Add(request.Uuid, game);  //TODO: Change This To Handle The Generated UUIDS (this should be a failure case normally)
+            }
 
+            context.UserState.Add("gameId", request.Uuid);
 
-                StatusResponse r = new StatusResponse();
-                r.Success = true;
-                r.ErrorNum = 0;
-                r.ErrorText = "";
+            Client c = new Client();
+            game.Clients.Add(c);
+            clients.Add(c.Token, new RpcClient(responseStream, c, game));
 
-                Notification n = new Notification();
-                n.Status = r;
+            // Send A Join Game Response
+            JoinGameResponse jgr = new JoinGameResponse();
+            jgr.Token = c.Token;
 
-                await responseStream.WriteAsync(n);                
+            Notification n = new Notification();
+            n.JoinResponse = jgr;
+
+            await responseStream.WriteAsync(n);                
+        
+            try {
+                await Task.Delay(-1, context.CancellationToken);
+            } catch (TaskCanceledException) {
+                // Task Cancelled, Do Any Disconnection Stuff
+                clients.Remove(c.Token);
+                game.Clients.Remove(c);
+            }
+
+        }
+      
+        private async void SendSeatsList(Game.Game game) {
+            SeatsList sl = new SeatsList();
+            for (int i = 0; i < game.NumPlayers; i++) {
+                Client c = game.Clients[i];
+                SeatDetails details = new SeatDetails();
+                details.Seat = (uint)i;
+                details.Ready = false;
+                details.Empty = (c == null);
+                details.Human = (c == null) ? false : c.Human;
+                details.Name = "Player " + i.ToString();
+                sl.Seats.Add(details);
+            }
+
+            Notification n = new Notification();
+            n.SeatList = sl;
+
+            foreach (Client c in game.Clients) {
+                try {
+                    RpcClient rpcClient = clients[c.Token];
+                    await rpcClient.Stream.WriteAsync(n);
+                } catch (KeyNotFoundException) {
+                }
             }
         }
+
+        public override Task<StatusResponse> TakeSeat(TakeSeatRequest request, ServerCallContext context)
+        {
+            string uuid = context.RequestHeaders.GetValue("x-game-id");
+            string clientToken = context.RequestHeaders.GetValue("x-game-token");
+            
+            StatusResponse r = new StatusResponse();
+            Game.Game game;
+
+            try {
+                game = games[uuid];
+            } catch (KeyNotFoundException) {
+                r.Success = false;
+                r.ErrorNum = 1; // TODO: Error Enum
+                r.ErrorText = "Game Not Found";
+                return Task.FromResult(r);
+            }
+
+            // Try To Take The Seat Request
+            Client seatClient = game.Players[request.Seat];
+            if (seatClient != null) {
+                r.Success = false;
+                r.ErrorNum = 2; // TODO: Error Enum
+                r.ErrorText = "Seat In Use";
+                return Task.FromResult(r);
+            } else {
+                RpcClient client = clients[clientToken];
+                game.Players[request.Seat] = client.Client;
+            }
+
+            r.Success = true;
+            r.ErrorNum = 0;
+            r.ErrorText = "";
+
+            SendSeatsList(game);
+
+            return Task.FromResult(r);
+        }
+
 
     }
 
--- frontend/shoot-client/src/App.tsx
+++ frontend/shoot-client/src/App.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import * as grpcWeb from 'grpc-web';
 import {ShootServerClient} from './ShootServiceClientPb';
-import {JoinGameRequest, Notification} from './shoot_pb';
+import {JoinGameRequest, Notification, StatusResponse, TakeSeatRequest} from './shoot_pb';
 import logo from './logo.svg';
 import './App.css';
 
-const stmService = new ShootServerClient('http://localhost:8080', null, null);
+const stmService = new ShootServerClient('http://localhost:8080', {"clientId": "my_client"}, null);
 
 function App() {
 
+  let token: string;
   const request = new JoinGameRequest();
-  request.setId('1234');
+  request.setUuid('1234');
 
-  const stream = stmService.joinGame(request, {} ) as grpcWeb.ClientReadableStream<Notification>;
+  const stream = stmService.joinGame(request, {'x-game-id': '1234'} ) as grpcWeb.ClientReadableStream<Notification>;
 
   stream.on('data', (response: Notification) => {
-    if (response.hasStatus()) {
+    if (response.hasJoinResponse()) {
+      token = response.getJoinResponse()?.getToken() as string;
+      console.log('got token: ' + token);
+      const tsreq = new TakeSeatRequest();
+      tsreq.setSeat(1);
+      stmService.takeSeat(tsreq, {'x-game-id': '1234', 'x-game-token': token}, (err: grpcWeb.Error, resp: StatusResponse) => {
+        console.log('take seat response: ');
+        console.log(resp.toObject());
+      });
+    } else if (response.hasStatus()) {
       console.log('status: ');
       console.log(response.getStatus()?.toObject());
     } else {
       console.log('data');
     }
-  });
+  });  
 
   return (
     <div className="App">
