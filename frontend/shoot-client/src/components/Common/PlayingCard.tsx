618b05af037ae161b8b5efc9cbab8c3354dc709d
deadwing88 <timbo145@gmail.com>
2021-05-05 18:42:47 -0400

    fix issues with styled components causing warnings

--- frontend/shoot-client/src/components/Common/PlayingCard.tsx
+++ frontend/shoot-client/src/components/Common/PlayingCard.tsx
@@ -45,25 +45,24 @@ function rankString(rank: Card.Rank): String {
     }
 }
 
+const StyledDiv = styled.div`
+    border-radius: 12px;
+    margin: 10px;
+    width: 55px;
+`;
+
+const RankDiv = styled.div`
+    font-size: 32px;
+    font-weight: bold;
+    padding: 6px 0px 4px 0px;
+`;
+
+const SuitDiv = styled.div`
+    font-size: 26px;
+    padding: 0px 0px 6px 0px;
+`;
+
 const PlayingCard: React.FC<IPlayingCardProps> = (props: IPlayingCardProps) => {
-
-    const StyledDiv = styled.div`
-        border-radius: 12px;
-        margin: 10px;
-        width: 55px;
-    `;
-
-    const RankDiv = styled.div`
-        font-size: 32px;
-        font-weight: bold;
-        padding: 6px 0px 4px 0px;
-    `;
-
-    const SuitDiv = styled.div`
-        font-size: 26px;
-        padding: 0px 0px 6px 0px;
-    `;
-
     return (  
         <StyledDiv>
             <CardView>
