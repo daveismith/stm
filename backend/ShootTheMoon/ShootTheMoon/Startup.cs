bedefa4b6e0422ecc5eeea833847857ad284696b
deadwing88 <timbo145@gmail.com>
2021-05-08 22:03:09 -0400

    rip swagger and api

--- backend/ShootTheMoon/ShootTheMoon/Startup.cs
+++ backend/ShootTheMoon/ShootTheMoon/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace ShootTheMoon
 {
@@ -20,11 +19,6 @@ namespace ShootTheMoon
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShootTheMoon", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,20 +30,7 @@ namespace ShootTheMoon
                 app.UseDeveloperExceptionPage();
             }
 
-            if (Configuration["ENABLE_SWAGGER"] == "true")
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShootTheMoon v1"));
-            }
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
         }
     }
 }
