58243bb2edfa9065b9d7a1acbe50906ec6db548e
deadwing88 <timbo145@gmail.com>
2021-06-22 20:47:18 -0400

    some progress on styling

--- frontend/shoot-client/src/components/Common/ButtonGroupSelector.tsx
+++ frontend/shoot-client/src/components/Common/ButtonGroupSelector.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, ButtonGroup } from "@material-ui/core";
 import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
 
 export interface IButtonGroupItem {
     value: string;
@@ -14,24 +15,46 @@ interface IButtonGroupSelectorProps {
     onClick(selectedValue: string) : void;
 }
 
-const StyledSpan = styled.span`
-    border-radius: 12px;
+const StyledDiv = styled.span`
     margin: 10px;
 `;
 
+const useStyles = makeStyles({
+    root: {
+      background: '#363636',
+      borderRadius: 30,
+      color: '#FFFFFF',
+      height: 48,
+      fontSize: '1.5rem',
+      '&$disabled': {
+        color: '#606060',
+      },
+    },
+    disabled: {},
+    selected: {},
+    label: {
+        height: 10,
+    }
+  });
+
 const ButtonGroupSelector: React.FC<IButtonGroupSelectorProps> = (props: IButtonGroupSelectorProps) => {
 
+    const classes = useStyles();
     const { name, items, selected, onClick } = props;
 
     return (  
-        <StyledSpan>
+        <StyledDiv>
             <ButtonGroup>
                 {
                     items.map((item, index) => (
                         <Button 
+                            classes={{
+                                root: classes.root,
+                                disabled: classes.disabled,
+                            }}
                             key={name + "_" + index}
                             disabled={item.disabled}
-                            color={item.value ===  selected ? "primary" : "secondary"}
+                            //color={item.value ===  selected ? "primary" : "secondary"}
                             onClick={() => onClick(item.value)}
                         >
                             {item.value}
@@ -39,7 +62,7 @@ const ButtonGroupSelector: React.FC<IButtonGroupSelectorProps> = (props: IButton
                     ))
                 }
             </ButtonGroup>
-        </StyledSpan>
+        </StyledDiv>
     );
 };
 
