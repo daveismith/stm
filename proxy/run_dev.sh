d8d92a56b377c41add5add9df055475b61238f91
David Smith <1734595+daveismith@users.noreply.github.com>
2024-11-12 20:59:16 -0500

    Updates to packages and envoy config

--- proxy/run_dev.sh
+++ proxy/run_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it --rm --name envoy -p 8080:8080 -v $(pwd)/envoy-dev.yaml:/etc/envoy/envoy.yaml envoyproxy/envoy:v1.31-latest
+docker run -it --rm --name envoy -p 8080:8080 -p 9901:9901 -v $(pwd)/envoy-dev.yaml:/etc/envoy/envoy.yaml -v $(pwd)/logs:/tmp/ envoyproxy/envoy:v1.31-latest
