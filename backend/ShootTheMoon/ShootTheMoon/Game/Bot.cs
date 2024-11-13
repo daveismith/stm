32c77c7844bb925c3d7ecf2934f91d29336e3a30
Tim Laengert <tim.laengert@protonmail.com>
2024-11-12 20:56:45 -0500

    add bots to game

--- backend/ShootTheMoon/ShootTheMoon/Game/Bot.cs
+++ backend/ShootTheMoon/ShootTheMoon/Game/Bot.cs
@@ -1,3 +1,4 @@
+using Grpc.Core;
 using ShootTheMoon.Network.Proto;
 
 namespace ShootTheMoon.Game
@@ -5,6 +6,8 @@ namespace ShootTheMoon.Game
     public class Bot : Client
     {
         private ShootServer.ShootServerClient _grpcClient;
+
+        public AsyncServerStreamingCall<Notification> NotificationStream  { get; set; }
 
         public Bot(ShootServer.ShootServerClient grpcClient) {
             _grpcClient = grpcClient;
