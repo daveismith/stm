089b7435810ea22b8179ec90955e4a164f4367b9
David Smith <1734595+daveismith@users.noreply.github.com>
2025-01-14 20:35:45 -0500

    Updates to fix package namespace conflict.

--- frontend/shoot-client/src/components/App/App.context.tsx
+++ frontend/shoot-client/src/components/App/App.context.tsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import * as grpcWeb from 'grpc-web';
-import { ShootServerClient } from 'grpc/ShootServiceClientPb';
+import { ShootServerClient } from 'proto/ShootServiceClientPb';
 import {
     CreateGameRequest,
     CreateGameResponse, 
     JoinGameRequest,
     Notification
-} from 'grpc/shoot_pb';
+} from 'proto/shoot_pb';
 
 export interface IApp {
     connection?: ShootServerClient
