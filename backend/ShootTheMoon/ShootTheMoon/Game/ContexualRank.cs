ae7d5880f907aaed8a75156539f8be992575cb37
Tim Laengert <tim.laengert@protonmail.com>
2025-02-18 20:58:37 -0500

    partial conversion of old bot code

--- backend/ShootTheMoon/ShootTheMoon/Game/ContexualRank.cs
+++ backend/ShootTheMoon/ShootTheMoon/Game/ContexualRank.cs
@@ -4,18 +4,6 @@ namespace ShootTheMoon.Game
 {
     public class ContextualRank
     {
-        // public static readonly List<ContextualRank> ContextualRanks = new List<ContextualRank>
-        // {
-        //     new ContextualRank {Ranking = 9, Name = "Nine" },
-        //     new ContextualRank {Ranking = 10, Name = "Ten" },
-        //     new ContextualRank {Ranking = 11, Name = "Jack" },
-        //     new ContextualRank {Ranking = 12, Name = "Queen" },
-        //     new ContextualRank {Ranking = 13, Name = "King" },
-        //     new ContextualRank {Ranking = 14, Name = "Ace" },
-        //     new ContextualRank {Ranking = 15, Name = "Left" },
-        //     new ContextualRank {Ranking = 16, Name = "Right" }
-        // };
-
         public static readonly ContextualRank NINE = new ContextualRank {Ranking = 9, Name = "Nine" };
 
         public static readonly ContextualRank TEN = new ContextualRank {Ranking = 10, Name = "Ten" };
@@ -31,6 +19,18 @@ namespace ShootTheMoon.Game
         public static readonly ContextualRank LEFT = new ContextualRank {Ranking = 15, Name = "Left" };
 
         public static readonly ContextualRank RIGHT = new ContextualRank {Ranking = 16, Name = "Right" };
+
+        public static readonly Dictionary<int, ContextualRank> ContextualRanks = new Dictionary<int, ContextualRank>
+        {
+            {9, NINE},
+            {10, TEN},
+            {11, JACK},
+            {12, QUEEN},
+            {13, KING},
+            {14, ACE},
+            {15, LEFT},
+            {16, RIGHT}
+        };
 
         public int Ranking { get; set; }
         public string Name { get; set; }
