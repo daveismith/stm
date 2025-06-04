c70f5207bf54f3d9d65062622666cffd080aa09e
Tim Laengert <tim.laengert@protonmail.com>
2025-06-03 20:59:52 -0400

    separate bot project

---
+++ backend/ShootTheMoon/Bot/Suit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    public class Suit
+    {
+        public static Suit Spades = new Suit {Index = 0, ShortName = "S", LongName = "Spades" };
+        public static Suit Hearts = new Suit {Index = 1, ShortName = "H", LongName = "Hearts" };
+        public static Suit Clubs = new Suit {Index = 3, ShortName = "C", LongName = "Clubs" };
+        public static Suit Diamonds = new Suit {Index = 2, ShortName = "D", LongName = "Diamonds" };
+
+        public static readonly Dictionary<int, Suit> Suits = new Dictionary<int, Suit>
+        {
+            {0, Spades},
+            {1, Hearts},
+            {2, Diamonds},
+            {3, Clubs}
+        };
+        public int Index { get; set; }
+
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+
+        public override bool Equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (!(o.GetType() == typeof(Suit)))
+            {
+                return false;
+            }
+            Suit s = (Suit)o;
+            return ShortName == s.ShortName;
+        }
+
+        public override int GetHashCode()
+        {
+            return ShortName.GetHashCode();
+        }
+    }
+}
---
+++ backend/ShootTheMoon/Bot/Trump.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    public class Trump
+    {
+        
+        public static Trump Spades = new Trump {Index = 0, Name = Suit.Spades.LongName, Suit = Suit.Spades, SameColour = Suit.Clubs };
+        public static Trump Hearts = new Trump {Index = 1, Name = Suit.Hearts.LongName, Suit = Suit.Hearts, SameColour = Suit.Diamonds };
+        public static Trump Clubs = new Trump {Index = 3, Name = Suit.Clubs.LongName, Suit = Suit.Clubs, SameColour = Suit.Spades };
+        public static Trump Diamonds = new Trump {Index = 2, Name = Suit.Diamonds.LongName, Suit = Suit.Diamonds, SameColour = Suit.Hearts };
+        public static Trump High = new Trump {Index = 5, Name = "High"};
+        public static Trump Low = new Trump {Index = 4, Name = "Low"};
+
+        public static readonly Dictionary<int, Trump> Trumps = new Dictionary<int, Trump>{
+            {0, Spades},
+            {1, Hearts},
+            {2, Diamonds},
+            {3, Clubs},
+            {4, Low},
+            {5, High}
+        };
+
+        public int Index { get; private set; }
+
+        public string Name { get; private set; }
+        public Suit Suit { get; private set; }
+
+        public Suit SameColour { get; private set; }
+
+        public bool isSuit() {
+            return Suit != null;
+        }
+
+        public static Trump fromProto(ShootTheMoon.Network.Proto.Trump trump)
+        {
+            return Trumps[(int) trump];
+        }
+
+        public static ShootTheMoon.Network.Proto.Trump toProto(Trump trump)
+        {
+            return (ShootTheMoon.Network.Proto.Trump) trump.Index; 
+        }
+    }
+}
