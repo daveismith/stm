19369c4581f51b8ae825d918490241baaa99920b
Tim Laengert <tim.laengert@protonmail.com>
2025-06-10 20:57:09 -0400

    bot join game

--- backend/ShootTheMoon/Bot/Program.cs
+++ backend/ShootTheMoon/Bot/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Bot;
+
 Console.WriteLine("Hello, World!");
+
+Bot.Bot bot = new Bot.Bot(BotProfile.DEFAULT);
+
+bot.JoinGame("second-hand_revolution");
+
+await bot.GetNotifications();
