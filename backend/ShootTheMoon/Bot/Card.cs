c324a3d84d10cf3397f104dbe77e026db1fe4814
Tim Laengert <tim.laengert@protonmail.com>
2025-08-12 20:55:51 -0400

    bot bidding

--- backend/ShootTheMoon/Bot/Card.cs
+++ backend/ShootTheMoon/Bot/Card.cs
@@ -12,7 +12,7 @@ namespace Bot
         public Rank Rank { get; set; }
 
         public Suit EffectiveSuit(Trump trump) {
-            if (Suit == trump.SameColour && Rank == Rank.Jack) {
+            if (trump.SameColour != null && Suit == trump.SameColour && Rank == Rank.Jack) {
                 return trump.Suit;
             }
             return Suit;
