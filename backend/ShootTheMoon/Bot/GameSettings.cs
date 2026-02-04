a86ccbadef40197518408ec15242d6c9c61f5a27
Tim Laengert <tim.laengert@protonmail.com>
2026-02-03 20:34:27 -0500

    fix cards per hand

--- backend/ShootTheMoon/Bot/GameSettings.cs
+++ backend/ShootTheMoon/Bot/GameSettings.cs
@@ -13,7 +13,7 @@ namespace Bot
             #if DEBUG
             { "TWOPLAYER", new GameSettings {NumPlayersPerTeam = 1, NumDuplicateCards = 1, ScoreNeededToWin = 51} },
             #endif
-            { "FOURPLAYER", new GameSettings {NumPlayersPerTeam = 2, NumDuplicateCards = 2, ScoreNeededToWin = 51} }, // To do: revert?
+            { "FOURPLAYER", new GameSettings {NumPlayersPerTeam = 2, NumDuplicateCards = 1, ScoreNeededToWin = 51} },
             { "SIXPLAYER", new GameSettings {NumPlayersPerTeam = 3, NumDuplicateCards = 2, ScoreNeededToWin = 51} },
             { "EIGHTPLAYER", new GameSettings {NumPlayersPerTeam = 4, NumDuplicateCards = 3, ScoreNeededToWin = 51} }
         };
